df6e384ef308aed7039d467b235013d1d8fcdb55
blejj k binks <romitdomange@gmail.com>
2024-10-11 17:14:30 +1030

    testing

--- testing/pipe.c
+++ testing/pipe.c
@@ -3,23 +3,36 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+#include "../libft/header/libft.h"
+
+void	ex_pipe(char **pipe)
+{
+	
+}
 
 int main()
 {
-	char *comm[10][10] = {
-	{"ls", NULL},
-	{"grep", "i", NULL},
-	{NULL}};
-/*	char *comm[10][10] = {{"cat", NULL}, {"cat", NULL}, {"ls", NULL}, NULL};*/
+	char	*s;
+	char	**pipe, **comm;
+//	s = "/bin/ls | /usr/bin/grep i";
+	s = "/bin/cat | /bin/cat | /bin/ls";
+//	s = "/bin/cat file | /usr/bin/grep bla | /usr/bin/more";
+//	s = "/bin/ls filethatdoesnotexist | /usr/bin/grep bla | /usr/binmore"
 
-	int i, j = 0;
-	for (i = 0; comm[i]; i++)
+	pipe = ft_split(s, "|");
+	for (int i = 0; p[i]; i++)
 	{
-		for (j = 0; comm[i][j]; j++)
+		comm = ft_split(p[i], " ");
+		for (int j = 0; c[j]; j++)
 		{
-			printf("%s, ", comm[i][j]);
+			printf("%s", c[j]);
+			if (c[j + 1])
+				printf(",");	
 		}
-		printf("\n");
+		ft_split_free(comm);
+		printf(";\n");
 	}
+	ex_pipe(pipe);
+	ft_split_free(pipe);
 	return (0);
 }
