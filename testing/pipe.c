891bbad277249f64f9cbd57c99be05626f9982da
blejj k binks <romitdomange@gmail.com>
2024-10-18 16:58:18 +1030

    at school

--- testing/pipe.c
+++ testing/pipe.c
@@ -1,15 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <string.h>
-#include "../libft/header/libft.h"
-
-typedef struct s_m
-{
-	char **pipe;
-}	t_m;
-
 void	print_comms(char **line)
 {
 	char	**comm;
@@ -32,72 +20,26 @@ void	print_comms(char **line)
 	printf("_________\n");
 }
 
-void	exec_pipe(char **line)
-{
-	int		i;
-	int		pipe_fd[2];
-	int		save_fd[2];
-	int		pid;
-	int		status;
-	char	**comm;
 
-	save_fd[0] = -1;
-	i = 0;
-	while (line[i])
-	{
-		printf("---i:%d\n", i);
-		comm = ft_split(line[i], " ");
-		if (line[i + 1])
-		{
-			pipe(pipe_fd);
-			printf("---pipe(pipe_fd): [0]:%d, [1]:%d\n", pipe_fd[0], pipe_fd[1]);
-		}
-		pid = fork();
-		if (pid == 0)
-		{
-			printf("---in child: pid=%d\n", pid);
-			if (save_fd[0] != -1)
-			{
-				dup2(save_fd[0], STDIN_FILENO);
-				close(save_fd[0]);
-			}
-			if (line[i + 1])
-			{
-				close(pipe_fd[0]);
-				dup2(pipe_fd[1], STDOUT_FILENO);
-				close(pipe_fd[1]);
-			}
-			execve(comm[0], comm, NULL);
-			exit (0 * printf("---execve didnt work\n"));
-		}
-		else
-		{
-			printf("---in parent: pid=%d\n", pid);
-			if (save_fd[0] != -1)
-				close(save_fd[0]);
-			if (line[i + 1])
-			{
-				close(pipe_fd[1]);
-				save_fd[0] = pipe_fd[0];
-			}
-			waitpid(pid, &status, 0);
-		}
-	i++;
-	}
-}
 
 int main()
 {
 	char	*s;
 	char	**line;
-	s = "/bin/ls | /usr/bin/grep i";
-//	s = "/bin/cat | /bin/cat | /bin/ls";
+	char	**redir_in;
+	char	**redir_out;
+
+//	s = "/bin/ls | /usr/bin/grep i";
+	s = "/bin/cat | /bin/cat | /bin/ls";
 //	s = "/bin/cat file | /usr/bin/grep bla | /usr/bin/more";
 //	s = "/bin/ls filethatdoesnotexist | /usr/bin/grep bla | /usr/bin/more";
-
 	line = ft_split(s, "|");
+
+	redir_in = 
+
 	print_comms(line);
 	exec_pipe(line);
+
 	ft_split_free(line);
 	return (0);
 }
