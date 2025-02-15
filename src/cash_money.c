a053506f6e50fca3a46dd7258b84d3636830cdd8
Romain Domange <rdomange@F2K004.42adel.org.au>
2025-02-15 18:46:29 +1030

    going home

--- src/cash_money.c
+++ src/cash_money.c
@@ -12,12 +12,70 @@
 
 #include "minishell.h"
 
-char	*cash_money(char *comm, char ***env, char *cash_q, char *last_c)
+//char **cash_money(pipe[i], env, cash_question, last_command);
+//also does split and trim_quotes at the end
+
+char	*cash_money(char *str, char ***env, char *cash_q, char *last_c)
 {
 	char	*ret;
+	char	*var;
 	int		i;
+	int		j;
+	int		q;
 
+	//get len for malloc
+	var = NULL;
+	i = 0;
+	j = 0;
+	q = 0;
+	while (str && str[i])
+	{
+		ft_isquoted(str[i], &q);
+		if (q != '\'')
+			var = cash_get_var(str, &i, env);
+		if (q != '\'' && !var)
+			var = cash_get_cash_q;
+		if (q != '\'' && !var)
+			var = cash_get_last_c;
+		if (var)
+			j += ft_strlcat();
+		else
+			ret[j++] = str[i++];
+		var = ft_free(val);
+	}
+	return (ret);
 }
+
+char	*cash_get_var(char *str, int *i, char ***env, char *cash_q)
+{
+	char	*ret;
+
+	ret = NULL;
+	if (str[*i] == '$' && ft_env_name(str[*i + 1], NULL))
+	{
+		ret = ft_env_get(env[0], str[*i + 1]);
+		if (!ret)
+			ret = ft_env_get(env[1], str[*i + 1]);
+		if (!ret)
+			ret = "";
+		*i += ft_env_name(str[*i + 1], NULL) + 1;
+	}
+	if (!ft_strncmp(str[*i], "$?", 2))
+	{
+		ret = cash_q;
+		*i += 2;
+	}
+	if ("~")
+	{
+		ret = ft_env_get(env[0], "HOME");
+		*i += 1;
+	}
+	if ("!!")
+		ret = last_c;
+
+	return (ft_strdup(ret));
+}
+
 //split and trim quotes here? and then return "char **comm"
 /*{
 	char	**ret;
