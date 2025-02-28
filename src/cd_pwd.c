4f784f0543982a8af4d413d0951388af2c13a89e
blejj k binks <romitdomange@gmail.com>
2025-02-28 23:09:22 +1030

    cd minus

--- src/cd_pwd.c
+++ src/cd_pwd.c
@@ -52,7 +52,7 @@ static int	ft_cd_oldpwd(char **arg, char **env, int err)
 		ft_printf("cd: no such file or directory: %s\n", arg[1]);
 	if (err == ENOTDIR)
 		ft_printf("cd: not a directory: %s\n", arg[1]);
-	if (!err)
+	if (!err && ft_strcmp(ft_env_get(env, "OLDPWD"), ft_env_get(env, "PWD")))
 		ft_env_set(env, "OLDPWD", ft_env_get(env, "PWD"));
 	str = ft_getcwd();
 	ft_env_set(env, "PWD", str);
